cb7b42cc8c7662cfa76ddb088ab88f76fc4bcd93
Pablo Santillana Fernandez <Pablo.Santillana@alu.uclm.es>
2024-10-30 19:01:52 +0200

    Update destino.java

--- listas/destino.java
+++ listas/destino.java
@@ -1,4 +1,5 @@
-package listas;
+package Listas;
+
 import java.util.*;
 import java.io.*;
 
@@ -22,8 +23,58 @@ public class destino{
 		this.popularidad=popularidad;
 		
 	}
+	public String getDestino() {
+		return Destino;
+	}
+
+	public void setDestino(String destino) {
+		Destino = destino;
+	}
+
+	public String getPais() {
+		return Pais;
+	}
+
+	public void setPais(String pais) {
+		Pais = pais;
+	}
+
+	public String getTipoDestino() {
+		return TipoDestino;
+	}
+
+	public void setTipoDestino(String tipoDestino) {
+		TipoDestino = tipoDestino;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public double getCoste() {
+		return coste;
+	}
+
+	public void setCoste(double coste) {
+		this.coste = coste;
+	}
+
+	public int getPopularidad() {
+		return popularidad;
+	}
+
+	public void setPopularidad(int popularidad) {
+		this.popularidad = popularidad;
+	}
+
 
 	public String toString() {
         return Destino + " (" + Pais + ")  " + TipoDestino + "  " + duracion + " días  " + coste + "€  Popularidad: " + popularidad;
     }
 }
+
+
