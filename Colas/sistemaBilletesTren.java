d8e004d7f7dea0d97a71e37e6a343dbeb79bd427
Yisuscrack210 <Jesus.Marquez1@alu.uclm.es>
2024-10-30 20:14:30 +0100

    Update sistemaBilletesTren.java

--- Colas/sistemaBilletesTren.java
+++ Colas/sistemaBilletesTren.java
@@ -1,24 +1,46 @@
 package Colas;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 public class sistemaBilletesTren{
 	public static void main(String[] args) throws FileNotFoundException {
-		int numBilletes=34;
-		billetesDeTren billetes[]=new billetesDeTren[numBilletes];
-		leerPeticiones(billetes,"D:\\ESI UCLM\\Segundo año\\2ºC\\Primer cuatrimestre\\Estructura de datos (EDA)\\Practicas\\Practica 3\\petitions.csv\"");
-		
-	}
-	public static void leerPeticiones(billetesDeTren [] t,String directorio) throws FileNotFoundException {
-		lecturaPeticiones <billetesDeTren> peticion=new lecturaPeticiones <billetesDeTren>("D:\\ESI UCLM\\Segundo año\\2ºC\\Primer cuatrimestre\\Estructura de datos (EDA)\\Practicas\\Practica 3\\petitions.csv\"");
-		while(peticion.finLectura()==false) {
-			for(int i=0;i<t.length;i++) {
-				peticion.leer(t[i]);
-				peticion.saltar();
-			}
-			if(peticion.finLectura()==true) {
-				peticion.cerrar();
+		lecturaPeticiones<billetesDeTren> fichero=new lecturaPeticiones <>("petitions.csv");
+		PriorityQueue<billetesDeTren> billetes=new PriorityQueue<>();
+		Queue<billetesDeTren> billetesNacionales=new LinkedBlockingQueue <>();
+		Queue<billetesDeTren> billetesInternacionales=new LinkedBlockingQueue<>();
+		int limInternacional=0;
+		int limNacional=0;
+		fichero.saltar();
+		billetesDeTren billete=new billetesDeTren();
+		System.out.println("Peticiones:");
+		while(fichero.finLectura()) {
+			fichero.leer(billete);
+			billetes.add(billete);
+			System.out.println(billete.toString());
+		}
+		System.out.println("\n");
+		billetesDeTren b[]=new billetesDeTren[billetes.size()];
+		for(int i=0;i<=b.length-1;i++){
+			b[i]=billetes.remove();
+		}
+		if(limInternacional!=20||limNacional!=30) {
+			for(int i=0;i<=b.length-1;i++) {
+				switch(b[i].getTipoDestino()){
+					case INTERNATIONAL:
+						billetesInternacionales.add(billete);
+						System.out.println("Billete internacional con información "+billete.toString()+" procesado correctamente");
+						limInternacional++;
+					break;
+					case NATIONAL:
+						billetesNacionales.add(billete);
+						System.out.println("Billete nacional con información "+billete.toString()+" procesado correctamente");
+						limNacional++;
+					break;
+				}
 			}
 		}
+		else
+			System.out.println("No se ha podido procesar el billete. Límite de billetes alcanzado");
+		fichero.cerrar();
 	}
-
 }
