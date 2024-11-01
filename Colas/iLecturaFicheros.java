75d109303c2114532d9083d11f86583817ec51e5
Yisuscrack210 <Jesus.Marquez1@alu.uclm.es>
2024-11-01 13:28:36 +0100

    Update iLecturaFicheros.java

--- Colas/iLecturaFicheros.java
+++ Colas/iLecturaFicheros.java
@@ -1,5 +1,21 @@
-package Colas;
-public interface iLecturaFicheros<T extends billetesDeTren> {
+/*
+ * Nombre de la clase: iLecturaFicheros
+ * Nombre del autor: Jesús Márquez Duque
+ * Fecha de creación: 09/10/2024
+ * Versión de la clase: 4.30.0
+ * Descripción de la clase: Interfaz del programa que, al ser implementada, se crea un método que guarde los datos del fichero leídos en un objeto/variable
+ */
+package Peticiones;
 
-	abstract void leerFichero(String[]s);
+public interface iLecturaFicheros {
+	/*
+	 * Nombre del método: leerFichero
+	 * Nombre del autor original: Jesús Márquez Duque
+	 * Descripcion del método: Al implemetarse, se guarda los datos del fichero leídos en un objeto/variable
+	 * Argumentos de llamada: Ninguno
+	 * Valor de retorno: Void, no devuelve nada, pues solo guarda datos
+	 * Archivos necesarios: Ninguno
+	 * La clase es pública (Al ser una interfaz), y abstarcta, pues ha de implemetarse en una clase, no en la interfaz
+	 */
+	abstract void leerFichero(String[] s);
 }
