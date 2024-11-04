e9b1207150e301d7e60b78b70f82ed008420a509
miiguel73 <miguel.mora4@alu.uclm.es>
2024-11-04 16:00:51 +0100

    Update PilaValorMin.java

--- Pilas/PilaValorMin.java
+++ Pilas/PilaValorMin.java
@@ -1,36 +1,42 @@
 package Pilas;
 import java.util.Stack;
-public class PilaValorMin extends Stack <Float> {
-	private Stack <Float> pilaMinima;
+public class PilaValorMin extends Stack<Float> {
+
+	private Stack<Float> pilaMinima;
+
 	public PilaValorMin() {
 		super();
-		pilaMinima=new Stack <>();
+		pilaMinima = new Stack<Float>();
 	}
-	public Float push(Float numero) {
-		if(pilaMinima.empty()) {
-			pilaMinima.push(numero);
-		}
-		else if(numero<=super.peek()) {
-			pilaMinima.push(numero);
-		}
-		super.push(numero);
-		return numero;
+
+	public Float push(Float num) {
+		if (empty())
+			pilaMinima.push(num);
+		else if (num <= pilaMinima.peek())
+			pilaMinima.push(num);
+		return super.push(num);
 	}
-	public void pop(Float numero) {
-		if(numero>super.peek()) {
+
+	public Float pop() {
+		Float valor = super.peek();
+		if (valor.equals(pilaMinima.peek())) {
 			pilaMinima.pop();
 		}
-		super.pop();
+		return valor;
+		
 	}
+
 	public Float min() {
 		return pilaMinima.peek();
+
 	}
+
 	public void partir() {
-		int mitad=size()/2;
-		for(int i=0; i<mitad;i++) {
+		int mitad = size() / 2;
+		for (int i = 0; i < mitad; i++)
 			pop();
-		}
 	}
+
 	public String toString() {
 		return "La pila auxiliar contiene los elementos: "+super.toString()+" y la de valor mínimo contiene"
 				+ "los elementos: "+pilaMinima;
