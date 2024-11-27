91888382dbe7c9b9c7b1feaef7d0ef66c8f6e872
Yisuscrack210 <Jesus.Marquez1@alu.uclm.es>
2024-11-27 17:42:03 +0100

    Update linea.java

--- grafos/linea.java
+++ grafos/linea.java
@@ -1,9 +1,17 @@
 package grafos;
+import graphsDS_ESI_UCLM_v2.*;
 
-public class linea implements iLecturaFichero {
-	public String estacion1;
-	public String estacion2;
-	public String numLinea;
+public class Linea implements iLecturaFichero,Identity {
+	private String estacion1;
+	private String estacion2;
+	private String numLineas;
+	private String ID;
+	public String getNumLineas() {
+		return numLineas;
+	}
+	public void setNumLineas(String numLineas) {
+		this.numLineas = numLineas;
+	}
 	public String getEstacion1() {
 		return estacion1;
 	}
@@ -16,19 +24,16 @@ public class linea implements iLecturaFichero {
 	public void setEstacion2(String estacion2) {
 		this.estacion2 = estacion2;
 	}
-	public String getNumLinea() {
-		return numLinea;
-	}
-	public void setNumLinea(String numLineas) {
-		this.numLinea = numLineas;
-	}
 	public String toString() {
-		return "Linea con Estacion1=" + estacion1 + ", estacion2=" + estacion2 + " y número de línea=" + numLinea ;
+		return "Linea con Estacion1=" + estacion1 + ", estacion2=" + estacion2+", y con "+numLineas+" líneas" ;
 	}
 	public void leerDatos(String []s) {
 		this.estacion1=s[0];
 		this.estacion2=s[1];
-		this.numLinea=s[2];
+		this.numLineas=s[2];
 	}
-	
+	public String getID() {
+		return ID;
+	}
 }
+
