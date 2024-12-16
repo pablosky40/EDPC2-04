1dd1edd3c7f1990c5a189c32748de4b01378a351
miiguel73 <miguel.mora4@alu.uclm.es>
2024-12-16 18:17:22 +0100

    Update competicionNBA.java

--- arboles/competicionNBA.java
+++ arboles/competicionNBA.java
@@ -1,3 +1,11 @@
+/*
+ * J_P_M_C2-04
+ * Nombre de la clase: CompeticionNBA
+ * Nombre de los autores: Jesús Márquez Duque, Miguel Mora Vega, Pablo Santillana Fernandez
+ * Fecha de creación: 13/11/2024
+ * Versión de la clase: 4.30.0
+ * Descripción de la clase: Clase pricipal
+ */
 package arboles;
 
 import treesDS_ESI_UCLM_v2.BinTree;
@@ -6,8 +14,16 @@ import treesDS_ESI_UCLM_v2.NodeBinTree;
 
 public class competicionNBA{
   public static void main(String args[]) {
-    BinTreeInterface<Integer> t=new BinTree<>();
-    NodeBinTree<Integer> nLak4 = new NodeBinTree<>(6);
+    /*
+		 * Nombre del método: main
+		 * Descripcion del método: Metodo principal que es donde se ejecuta el programa
+		 * Argumentos de llamada: 
+			args: Vector de cadenas de caracteres que indica los caracteres
+		 * Valor de retorno: Void, no devuelvo nada, pues solo ejecuta
+		 * Archivos necesarios: Ninguno
+		 */
+    BinTreeInterface<Integer> t=new BinTree<>(); //Creamos un nuevo árbol t pertenciente a la librería importada
+    NodeBinTree<Integer> nLak4 = new NodeBinTree<>(6);// Añadimos el número de victorias a cada equipo y su fase con el DNI de mayor valor
     NodeBinTree<Integer> nLak2 = new NodeBinTree<>(7);
     NodeBinTree<Integer> nMia4 = new NodeBinTree<>(5);
     NodeBinTree<Integer> nBos4 = new NodeBinTree<>(5);
@@ -23,7 +39,7 @@ public class competicionNBA{
     NodeBinTree<Integer> nMem2=new NodeBinTree<>(9);
     NodeBinTree<Integer> nMem4=new NodeBinTree<>(5);
     
-    t.addRoot(nMemW);
+    t.addRoot(nMemW); //añadimos los nodos al árbol según la imagen del enunciado comenzando por el ganador (la raíz)
     t.addLeft(nMemW,nBos1);
     t.addRight(nMemW, nMem1);
     t.addLeft(nBos1, nLak2);
@@ -40,13 +56,20 @@ public class competicionNBA{
     t.addLeft(nLak2, nLak4);
     //
 
-    System.out.println("Mejor conferencia: "+mejorConferencia(t));
-    System.out.println("El número de nodos es: "+t.size());
+    System.out.println("Mejor conferencia: "+mejorConferencia(t));//Sacamos por pantalla el resultado de la conferencia con más victorias
+    System.out.println("El número de nodos es: "+t.size());//Sacamos por pantalla el número total de nodos del arbol
   }
   
   private static String mejorConferencia(BinTreeInterface<Integer> t)
   {
-  	int vOeste=sumarVictorias(t,t.getRoot().getLeft()),
+    /*
+		 * Nombre del método: mejorConferencia
+		 * Descripcion del método: método que devuelve "Este" u "oeste" según el número de victorias de cada conferencia
+		 * Argumentos de llamada: t, el arbol creado anteriormente
+		 * Valor de retorno: String, cadena de caracteres
+		 * Archivos necesarios: Ninguno
+		 */
+  	int vOeste=sumarVictorias(t,t.getRoot().getLeft()),//variables que almacenan las victorias de cada conferencia
   			vEste=sumarVictorias(t,t.getRoot().getRight());
 
   	if(vOeste>vEste) 
@@ -57,11 +80,18 @@ public class competicionNBA{
 
   private static int sumarVictorias(BinTreeInterface<Integer> t, NodeBinTree<Integer> nodo)
   {
-  	int sv=0;
-    if(t.isExternal(nodo))
+    /*
+		 * Nombre del método: sumarVictorias
+		 * Descripcion del método: método que devuelve el número de victorias de una conferencia
+		 * Argumentos de llamada: t, el arbol creado anteriormente y los nodos del árbol
+		 * Valor de retorno: int
+		 * Archivos necesarios: Ninguno
+		 */
+  	int sv=0; //contador de victorias
+    if(t.isExternal(nodo)) //si el nodo es una hoja solo cuenta el suyo
     	sv=nodo.getElement();
-    else if(t.isInternal(nodo))
-    	sv=nodo.getElement()+nodo.getLeft().getElement()+nodo.getRight().getElement();
+    else if(t.isInternal(nodo)) //si el nodo es interno se cuenta tanto el suyo como el de sus hijos
+      sv=nodo.getElement()+nodo.getLeft().getElement()+nodo.getRight().getElement();
     return sv;
   }
 }
