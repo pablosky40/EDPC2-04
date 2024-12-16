b17dc80cd81da2aaf7ca1786b398132f87addbdf
Yisuscrack210 <Jesus.Marquez1@alu.uclm.es>
2024-12-16 15:01:51 +0100

    Update Visitar.java

--- grafos/Visitar.java
+++ grafos/Visitar.java
@@ -1,40 +1,80 @@
-package P5;
-import graphsDS_ESI_UCLM_v2.*; 
-
-// Clase que extiende la funcionalidad de un objeto con Decorator
-public class Visitar<DA> extends Decorator {
-	private boolean visitado; // Indicaremos si es true o falsevisitado
-	private Visitar padre; // Respecto al  nodo o elemento padre en el recorrido
-
-	// Constructor que inicializa la clase con los datos y establece valores predeterminados
+/*
+ * J_P_M_C2-04
+ * Nombre de la clase: Visitar
+ * Nombre de los autores: Jesús Márquez Duque, Miguel Mora Vega, Pablo Santillana Fernandez
+ * Fecha de creación: 13/11/2024
+ * Versión de la clase: 4.30.0
+ * Descripción de la clase: Indica si el vértice del grafo ha sido visitado al hacer BFS
+ */
+package grafos;
+import graphsDS_ESI_UCLM_v2.*;//Importa los elementos de la librería de grafos graphsDS_ESI_UCLM_v2
+public class Visitar<DA> extends Decorator {//Dicha clase, con una variable generica, hereda de la clase Decorator de grafos
+	private boolean visitado;//Variable privada booleana que indica que el vertice ha sido visitado
+	private Visitar padre;//Variable privada del tipo de la clase que indica el vertice
+	/*
+	 * Nombre del método: Visitar
+	 * Descripcion del método: Constructor de la clase, que es como se va a inicializar
+	 * Argumentos de llamada: 
+		datos: DA, es decir, del tipo genérico
+	 * Valor de retorno: Void, no devuelvo nada, pues es el constructor
+	 * Archivos necesarios: Ninguno
+	 */
 	public Visitar(DA datos) {
-		super(datos); // Llama al constructor de la clase base Decorator
-		visitado = false; // Inicialmente, el nodo esta en false
-		padre = null; // Inicialmente, el nodo no tiene un padre
+		super(datos);//Coge la variable de la clase Decortator, que es la que hereda
+		visitado=false;//Inicializa la variable visitado a falsa
+		padre=null;//Inicializa la variable padre como vacía
 	}
-
-	// Método para verificar si el nodo ha sido visitado
+	/*
+	 * Nombre del método: isVisitado
+	 * Descripcion del método: Obtiene el valor de la variable visitado
+	 * Argumentos de llamada: Ninguno
+	 * Valor de retorno: boolean, pues devuelve el tipo de variable que se quiere obtener
+	 * Archivos necesarios: Ninguno
+	 */
 	public boolean isVisitado() {
 		return visitado;
 	}
-
-	// Método para marcar el nodo como visitado o no visitado
+	/*
+	 * Nombre del método: setVisitado
+	 * Descripcion del método: Actualiza el valor de la variable visitado
+	 * Argumentos de llamada: 
+	 *		visitado: boolean que indica el valor que se desea actualizar
+	 * Valor de retorno: Void, pues solo actualiza
+	 * Archivos necesarios: Ninguno
+	 */
 	public void setVisitado(boolean visitado) {
 		this.visitado = visitado;
 	}
-
-	// Método para obtener el nodo padre
+	/*
+	 * Nombre del método: getPadre
+	 * Descripcion del método: Obtiene el valor de la variable padre
+	 * Argumentos de llamada: Ninguno
+	 * Valor de retorno: Visitar, pues devuelve el tipo de variable que se quiere obtener
+	 * Archivos necesarios: Ninguno
+	 */
 	public Visitar getPadre() {
 		return padre;
 	}
-
-	// Método para establecer el nodo padre
+	/*
+	 * Nombre del método: setPadre
+	 * Descripcion del método: Actualiza el valor de la variable padre
+	 * Argumentos de llamada: 
+	 *		padre: Visitar que indica el valor que se desea actualizar
+	 * Valor de retorno: Void, pues solo actualiza
+	 * Archivos necesarios: Ninguno
+	 */
 	public void setPadre(Visitar padre) {
 		this.padre = padre;
 	}
-
-	// Método que devuelve una representación en texto del estado de visita
+	/*
+	 * Nombre del método: toString
+	 * Descripcion del método: Representa en una cadena de caracteres lo que tiene la clase
+	 * Argumentos de llamada: Nunguno
+	 * Valor de retorno: String, pues devuelve una cadena de caracteres con lo que tiene la clase
+	 * Archivos necesarios: Ninguno
+	 */
 	public String toString() {
-		return visitado + ""; // Convierte el valor booleano visitado a una cadena
+		return visitado+"";
 	}
+	
 }
